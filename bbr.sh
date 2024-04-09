ca7c3d28d7adcece2c026c9477e52d7a52e87f57
passeway <164740303+passeway@users.noreply.github.com>
2024-04-09 23:32:22 +0800

    更新 bbr.sh

--- bbr.sh
+++ bbr.sh
@@ -1,27 +1,25 @@
 #!/bin/bash
 
-tcp_tune() {
-    # 优化TCP窗口
-    sed -i '/net.ipv4.tcp_no_metrics_save/d' /etc/sysctl.conf
-    sed -i '/net.ipv4.tcp_ecn/d' /etc/sysctl.conf
-    sed -i '/net.ipv4.tcp_frto/d' /etc/sysctl.conf
-    sed -i '/net.ipv4.tcp_mtu_probing/d' /etc/sysctl.conf
-    sed -i '/net.ipv4.tcp_rfc1337/d' /etc/sysctl.conf
-    sed -i '/net.ipv4.tcp_sack/d' /etc/sysctl.conf
-    sed -i '/net.ipv4.tcp_fack/d' /etc/sysctl.conf
-    sed -i '/net.ipv4.tcp_window_scaling/d' /etc/sysctl.conf
-    sed -i '/net.ipv4.tcp_adv_win_scale/d' /etc/sysctl.conf
-    sed -i '/net.ipv4.tcp_moderate_rcvbuf/d' /etc/sysctl.conf
-    sed -i '/net.ipv4.tcp_rmem/d' /etc/sysctl.conf
-    sed -i '/net.ipv4.tcp_wmem/d' /etc/sysctl.conf
-    sed -i '/net.core.rmem_max/d' /etc/sysctl.conf
-    sed -i '/net.core.wmem_max/d' /etc/sysctl.conf
-    sed -i '/net.ipv4.udp_rmem_min/d' /etc/sysctl.conf
-    sed -i '/net.ipv4.udp_wmem_min/d' /etc/sysctl.conf
-    sed -i '/net.core.default_qdisc/d' /etc/sysctl.conf
-    sed -i '/net.ipv4.tcp_congestion_control/d' /etc/sysctl.conf
-    
-    cat >> /etc/sysctl.conf << EOF
+tcp_tune(){ # 优化TCP窗口
+sed -i '/net.ipv4.tcp_no_metrics_save/d' /etc/sysctl.conf
+sed -i '/net.ipv4.tcp_ecn/d' /etc/sysctl.conf
+sed -i '/net.ipv4.tcp_frto/d' /etc/sysctl.conf
+sed -i '/net.ipv4.tcp_mtu_probing/d' /etc/sysctl.conf
+sed -i '/net.ipv4.tcp_rfc1337/d' /etc/sysctl.conf
+sed -i '/net.ipv4.tcp_sack/d' /etc/sysctl.conf
+sed -i '/net.ipv4.tcp_fack/d' /etc/sysctl.conf
+sed -i '/net.ipv4.tcp_window_scaling/d' /etc/sysctl.conf
+sed -i '/net.ipv4.tcp_adv_win_scale/d' /etc/sysctl.conf
+sed -i '/net.ipv4.tcp_moderate_rcvbuf/d' /etc/sysctl.conf
+sed -i '/net.ipv4.tcp_rmem/d' /etc/sysctl.conf
+sed -i '/net.ipv4.tcp_wmem/d' /etc/sysctl.conf
+sed -i '/net.core.rmem_max/d' /etc/sysctl.conf
+sed -i '/net.core.wmem_max/d' /etc/sysctl.conf
+sed -i '/net.ipv4.udp_rmem_min/d' /etc/sysctl.conf
+sed -i '/net.ipv4.udp_wmem_min/d' /etc/sysctl.conf
+sed -i '/net.core.default_qdisc/d' /etc/sysctl.conf
+sed -i '/net.ipv4.tcp_congestion_control/d' /etc/sysctl.conf
+cat >> /etc/sysctl.conf << EOF
 net.ipv4.tcp_no_metrics_save=1
 net.ipv4.tcp_ecn=0
 net.ipv4.tcp_frto=0
@@ -41,9 +39,5 @@ net.ipv4.udp_wmem_min=8192
 net.core.default_qdisc=fq
 net.ipv4.tcp_congestion_control=bbr
 EOF
-
-    sysctl -p && sysctl --system
+sysctl -p && sysctl --system
 }
-
-# 调用函数
-tcp_tune
