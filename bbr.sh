f7c93c6a16e1fe7d5b216fb41d0384363220bea3
passeway <164740303+passeway@users.noreply.github.com>
2024-10-25 18:58:26 +0800

    Update bbr.sh

--- bbr.sh
+++ bbr.sh
@@ -1,33 +1,43 @@
 #!/bin/bash
 
 tcp_tune() {
+    # 备份原有的 sysctl.conf
+    cp /etc/sysctl.conf /etc/sysctl.conf.bak.$(date +%Y%m%d_%H%M%S)
+
+    # 要删除的 TCP 配置项
+    tcp_params=(
+        "net.ipv4.tcp_no_metrics_save"
+        "net.ipv4.tcp_ecn"
+        "net.ipv4.tcp_frto"
+        "net.ipv4.tcp_mtu_probing"
+        "net.ipv4.tcp_rfc1337"
+        "net.ipv4.tcp_sack"
+        "net.ipv4.tcp_fack"
+        "net.ipv4.tcp_window_scaling"
+        "net.ipv4.tcp_adv_win_scale"
+        "net.ipv4.tcp_moderate_rcvbuf"
+        "net.ipv4.tcp_rmem"
+        "net.ipv4.tcp_wmem"
+        "net.core.rmem_max"
+        "net.core.wmem_max"
+        "net.ipv4.udp_rmem_min"
+        "net.ipv4.udp_wmem_min"
+        "net.core.default_qdisc"
+        "net.ipv4.tcp_congestion_control"
+    )
+
     # 删除现有的 TCP 配置
-    sed -i '/net.ipv4.tcp_no_metrics_save/d' /etc/sysctl.conf
-    sed -i '/net.ipv4.tcp_ecn/d' /etc/sysctl.conf
-    sed -i '/net.ipv4.tcp_frto/d' /etc/sysctl.conf
-    sed -i '/net.ipv4.tcp_mtu_probing/d' /etc/sysctl.conf
-    sed -i '/net.ipv4.tcp_rfc1337/d' /etc/sysctl.conf
-    sed -i '/net.ipv4.tcp_sack/d' /etc/sysctl.conf
-    sed -i '/net.ipv4.tcp_fack/d' /etc/sysctl.conf
-    sed -i '/net.ipv4.tcp_window_scaling/d' /etc/sysctl.conf
-    sed -i '/net.ipv4.tcp_adv_win_scale/d' /etc/sysctl.conf
-    sed -i '/net.ipv4.tcp_moderate_rcvbuf/d' /etc/sysctl.conf
-    sed -i '/net.ipv4.tcp_rmem/d' /etc/sysctl.conf
-    sed -i '/net.ipv4.tcp_wmem/d' /etc/sysctl.conf
-    sed -i '/net.core.rmem_max/d' /etc/sysctl.conf
-    sed -i '/net.core.wmem_max/d' /etc/sysctl.conf
-    sed -i '/net.ipv4.udp_rmem_min/d' /etc/sysctl.conf
-    sed -i '/net.ipv4.udp_wmem_min/d' /etc/sysctl.conf
-    sed -i '/net.core.default_qdisc/d' /etc/sysctl.conf
-    sed -i '/net.ipv4.tcp_congestion_control/d' /etc/sysctl.conf
+    for param in "${tcp_params[@]}"; do
+        sed -i "/$param/d" /etc/sysctl.conf
+    done
 
     # 应用新的 TCP 优化配置
     cat >> /etc/sysctl.conf << EOF
 net.ipv4.tcp_no_metrics_save=1
 net.ipv4.tcp_ecn=0
-net.ipv4.tcp_frto=0
-net.ipv4.tcp_mtu_probing=0
-net.ipv4.tcp_rfc1337=0
+net.ipv4.tcp_frto=1
+net.ipv4.tcp_mtu_probing=1
+net.ipv4.tcp_rfc1337=1
 net.ipv4.tcp_sack=1
 net.ipv4.tcp_fack=1
 net.ipv4.tcp_window_scaling=1
@@ -43,11 +53,13 @@ net.core.default_qdisc=fq
 net.ipv4.tcp_congestion_control=bbr
 EOF
 
-    # 应用更改，检查错误
-    if sysctl -p > /dev/null 2>&1 && sysctl --system > /dev/null 2>&1; then
-        echo "TCP 优化配置已成功应用，并已开启BBR"
+    # 只使用 sysctl -p 重新加载配置文件
+    if sysctl -p > /dev/null 2>&1; then
+        echo "TCP 优化配置已成功应用，并已开启 BBR"
     else
-        echo "应用 TCP 优化配置时发生错误"
+        echo "应用 TCP 优化配置时发生错误，恢复之前的配置"
+        cp /etc/sysctl.conf.bak.$(date +%Y%m%d_%H%M%S) /etc/sysctl.conf
+        sysctl -p > /dev/null 2>&1  # 恢复配置
     fi
 }
 
