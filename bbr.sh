00352ad51feb76df1cfdfbd18e2dc424eac8c711
passeway <164740303+passeway@users.noreply.github.com>
2024-11-20 11:37:34 +0800

    Update bbr.sh

--- bbr.sh
+++ bbr.sh
@@ -38,26 +38,26 @@ tcp_tune() {
     done
 
     # 应用新的 TCP 优化配置
-    cat >> /etc/sysctl.conf <<-EOF
-        net.ipv4.tcp_no_metrics_save=1
-        net.ipv4.tcp_ecn=0
-        net.ipv4.tcp_frto=1
-        net.ipv4.tcp_mtu_probing=1
-        net.ipv4.tcp_rfc1337=1
-        net.ipv4.tcp_sack=1
-        net.ipv4.tcp_fack=1
-        net.ipv4.tcp_window_scaling=1
-        net.ipv4.tcp_adv_win_scale=1
-        net.ipv4.tcp_moderate_rcvbuf=1
-        net.core.rmem_max=33554432
-        net.core.wmem_max=33554432
-        net.ipv4.tcp_rmem=4096 87380 33554432
-        net.ipv4.tcp_wmem=4096 16384 33554432
-        net.ipv4.udp_rmem_min=8192
-        net.ipv4.udp_wmem_min=8192
-        net.core.default_qdisc=fq
-        net.ipv4.tcp_congestion_control=bbr
-    EOF
+    cat >> /etc/sysctl.conf <<EOF
+net.ipv4.tcp_no_metrics_save=1
+net.ipv4.tcp_ecn=0
+net.ipv4.tcp_frto=1
+net.ipv4.tcp_mtu_probing=1
+net.ipv4.tcp_rfc1337=1
+net.ipv4.tcp_sack=1
+net.ipv4.tcp_fack=1
+net.ipv4.tcp_window_scaling=1
+net.ipv4.tcp_adv_win_scale=1
+net.ipv4.tcp_moderate_rcvbuf=1
+net.core.rmem_max=33554432
+net.core.wmem_max=33554432
+net.ipv4.tcp_rmem=4096 87380 33554432
+net.ipv4.tcp_wmem=4096 16384 33554432
+net.ipv4.udp_rmem_min=8192
+net.ipv4.udp_wmem_min=8192
+net.core.default_qdisc=fq
+net.ipv4.tcp_congestion_control=bbr
+EOF
 
     # 重新加载 sysctl 配置
     if sysctl -p > /dev/null 2>&1; then
