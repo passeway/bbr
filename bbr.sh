7aeadf4c5b4386eafcf8c31b5d98df8f1a98147d
passeway <164740303+passeway@users.noreply.github.com>
2024-11-23 17:35:26 +0800

    Update bbr.sh

--- bbr.sh
+++ bbr.sh
@@ -10,8 +10,6 @@ tcp_tune() {
         exit 1
     fi
 
-    echo "备份已创建：$backup_file"
-
     # 要删除的 TCP 配置项
     tcp_params=(
         "net.ipv4.tcp_no_metrics_save"
@@ -34,13 +32,12 @@ tcp_tune() {
         "net.ipv4.tcp_congestion_control"
     )
 
-    echo "正在删除现有 TCP 配置..."
+    # 删除现有的 TCP 配置
     for param in "${tcp_params[@]}"; do
         sed -i -e "/$param/d" /etc/sysctl.conf
-        echo "已删除 $param"
     done
 
-    echo "应用新的 TCP 优化配置..."
+    # 应用新的 TCP 优化配置
     cat >> /etc/sysctl.conf <<EOF
 net.ipv4.tcp_no_metrics_save=1
 net.ipv4.tcp_ecn=0
@@ -61,22 +58,20 @@ net.ipv4.udp_wmem_min=8192
 net.core.default_qdisc=fq
 net.ipv4.tcp_congestion_control=bbr
 EOF
-    echo "新配置已写入 /etc/sysctl.conf"
 
-    echo "重新加载 sysctl 配置..."
-    if sysctl -p; then
-        echo "sysctl 配置加载成功"
+    # 重新加载 sysctl 配置
+    if sysctl -p > /dev/null 2>&1; then
+        # 检查 BBR 是否成功启用
         if sysctl net.ipv4.tcp_congestion_control | grep -q "bbr"; then
-            echo "BBR 拥塞控制算法已成功启用"
+            echo "bbr 拥塞控制算法成功启用"
         else
-            echo "BBR 启用失败，请检查配置"
+            echo "bbr 启用失败，请检查配置"
             exit 1
         fi
     else
-        echo "应用 TCP 优化配置时发生错误，正在恢复之前的配置..."
+        echo "应用 TCP 优化配置时发生错误，已恢复之前的配置"
         cp "$backup_file" /etc/sysctl.conf
-        sysctl -p
-        echo "配置已恢复至备份版本"
+        sysctl -p > /dev/null 2>&1  # 恢复配置
         exit 1
     fi
 }
